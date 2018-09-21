d733d2271615876a6dfe0f8cf66bc7d3a0362334
朱文赵 <zwz@servyou.com.cn>
2018-09-21 12:30:23 +0800

    打印树节点信息 和 输出倒叙数据链表

---
+++ sword-pointing-to-offer/src/main/java/com/ximo/sword/question6/PrintTree.java
@@ -0,0 +1,28 @@
+package com.ximo.sword.question6;
+
+/**
+ * @author 朱文赵
+ * @date 2018/9/21 12:26
+ * @description
+ */
+public class PrintTree {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    private class TreeNode{
+        private char data;
+
+        private TreeNode left;
+
+        private TreeNode right;
+
+
+    }
+
+
+
+}
